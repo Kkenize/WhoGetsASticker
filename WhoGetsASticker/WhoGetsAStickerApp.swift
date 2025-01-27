9d4f06ca2d595d6ccd2dc1276376187dca868be3
Kkenize <Kkenizez@gmail.com>
2025-01-27 16:53:17 -0500

    Initial Commit

---
+++ WhoGetsASticker/WhoGetsAStickerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WhoGetsAStickerApp.swift
+//  WhoGetsASticker
+//
+//  Created by Zhejun Zhang on 1/27/25.
+//
+
+import SwiftUI
+
+@main
+struct WhoGetsAStickerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
